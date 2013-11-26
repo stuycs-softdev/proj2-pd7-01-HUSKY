e4b098dd04ad7b4507bb5ddd1bbbb4f669d26431
Hanson Lin <hanson.lin1@gmail.com>
2013-11-26 11:47:40 -0500

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import urllib
 import urllib2
 import sys
 
-page = urllib2.urlopen("http://en.wikipedia.org/w/api.php?action=query&prop=revisions&format=json&rvprop=content&rvlimit=1&titles=Sicilian_Defense")
+page = urllib2.urlopen("http://en.wikipedia.org/w/api.php?action=query&prop=revisions&format=json&rvprop=content&rvlimit=1&titles=Sicilian_Defence")
 
 test = json.load(page)
 print(test)
