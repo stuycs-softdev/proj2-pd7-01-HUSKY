9a9adc6dd47acf2b8808eb141010efb047cfa6e0
Hanson Lin <hanson.lin1@gmail.com>
2013-11-26 11:46:24 -0500

    Update test.py

--- test.py
+++ test.py
@@ -5,5 +5,5 @@ import sys
 
 page = urllib2.urlopen("http://en.wikipedia.org/w/api.php?action=query&prop=revisions&format=json&rvprop=content&rvlimit=1&titles=Sicilian_Defense")
 
-test = json.load(page);
-print(test);
+test = json.load(page)
+print(test)
