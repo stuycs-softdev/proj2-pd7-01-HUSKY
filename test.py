53c2e4d3941fd1900a74cbd0b356f45e4a882531
Hanson Lin <hanson.lin1@gmail.com>
2013-11-26 11:49:34 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import json
-import urllib
+from bs4 import bs4
 import urllib2
 import sys
+
 
 page = urllib2.urlopen("http://en.wikipedia.org/w/api.php?action=query&prop=revisions&format=json&rvprop=content&rvlimit=1&titles=Sicilian_Defence")
 
