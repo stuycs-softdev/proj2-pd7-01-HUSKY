f60016352d5476882135dc979eb8a3ea7117c362
Nancy Dai <babycarr6193@gmail.com>
2013-12-01 15:13:57 -0500

    add templates

--- app.py
+++ app.py
@@ -13,19 +13,29 @@ def home():
 
 @app.route('/converter/<link>', methods = ['GET', 'POST'])
 def convert(link):
-    if request.method == "GET":
-        d = cbassfunc_to_return_only_original_list
-        return render_template("convert.html", d)
-    else:
-        if request.form['button'] == "Descriptive":
-            d = cbassfun_to_return_Descriptive_list
-            return render_template("convert.html", d)
-        else if request.form['button'] == "original":
-            d = cbassfunc_for only original
-            return render_template("convert.html", d)
+    try:
+        if checklink(link):
+            if request.method == "GET":
+                d = cbassfunc_to_return_only_original_list
+                return render_template("convert.html", d=d, link=link, m="o")
+            else:
+                if request.form['button'] == "Descriptive":
+                    d = cbassfun_to_return_Descriptive_list
+                    return render_template("convert.html", d=d, link=link, m="d")
+                elif request.form['button'] == "original":
+                    d = cbassfunc_for only original
+                    return render_template("convert.html", d=d, link=link, m="o")
+                else:
+                    d = cbassfunc_to_return_both_notations
+                    return render_template("convert.html", d=d, link=link, m="b")
         else:
-            d = cbassfunc_to_return_both_notations
-            return render_template("convert.html", d)
+            return render_template("noconvert.html", url=link)
+    except:
+        return redirect(url_for('home'))
+
+def checklink(link):
+    d = api.findNot(link)
+    return len(0) != 0
 
 if __name__ == "__main__":
     app.debug = True
