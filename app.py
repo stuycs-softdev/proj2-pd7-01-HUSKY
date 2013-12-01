909712813207d957f6ed37d2bfaf9a253c858a57
Nancy Dai <babycarr6193@gmail.com>
2013-12-01 14:18:46 -0500

    app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect
+import api
 
 app = Flask(__name__)
 
@@ -7,9 +8,24 @@ app = Flask(__name__)
 def home():
     if request.method == "GET":
         return render_template("home.html")
-    
-#@app.route('/moves/<link>', methods = ['GET', 'POST'])
-#def moves(link):
+    else:
+        convert(request.form["url"])
+
+@app.route('/converter/<link>', methods = ['GET', 'POST'])
+def convert(link):
+    if request.method == "GET":
+        d = cbassfunc_to_return_only_original_list
+        return render_template("convert.html", d)
+    else:
+        if request.form['button'] == "Descriptive":
+            d = cbassfun_to_return_Descriptive_list
+            return render_template("convert.html", d)
+        else if request.form['button'] == "original":
+            d = cbassfunc_for only original
+            return render_template("convert.html", d)
+        else:
+            d = cbassfunc_to_return_both_notations
+            return render_template("convert.html", d)
 
 if __name__ == "__main__":
     app.debug = True
