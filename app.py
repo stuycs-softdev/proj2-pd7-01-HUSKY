f4ce9f0c5fb20605872bbbabd6ef391ff201f8f3
Nancy Dai <babycarr6193@gmail.com>
2013-12-01 16:01:29 -0500

    changed func names

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect
 import api
+import converter
 
 app = Flask(__name__)
 
@@ -15,27 +16,25 @@ def home():
 def convert(link):
     try:
         if checklink(link):
+            a = api.findNotation(link)
+            d = convertNotation(a)
             if request.method == "GET":
-                d = cbassfunc_to_return_only_original_list
-                return render_template("convert.html", d=d, link=link, m="o")
+                return render_template("convert.html", d=d, link=link, m="o", a=a)
             else:
                 if request.form['button'] == "Descriptive":
-                    d = cbassfun_to_return_Descriptive_list
-                    return render_template("convert.html", d=d, link=link, m="d")
+                    return render_template("convert.html", d=d, link=link, m="d", a=a)
                 elif request.form['button'] == "original":
-                    d = cbassfunc_for only original
-                    return render_template("convert.html", d=d, link=link, m="o")
+                    return render_template("convert.html", d=d, link=link, m="o", a=a)
                 else:
-                    d = cbassfunc_to_return_both_notations
-                    return render_template("convert.html", d=d, link=link, m="b")
+                    return render_template("convert.html", d=d, link=link, m="b", a=a)
         else:
             return render_template("noconvert.html", url=link)
     except:
         return redirect(url_for('home'))
 
 def checklink(link):
-    d = api.findNotation(link)
-    return len(0) != 0
+    a = api.findNotation(link)
+    return len(a) != 0
 
 if __name__ == "__main__":
     app.debug = True
