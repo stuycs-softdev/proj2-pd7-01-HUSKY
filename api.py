70c68ec164b96b7c79289a83830a2e1948136909
Nancy Dai <babycarr6193@gmail.com>
2013-12-01 13:20:26 -0500

    text grabing

--- api.py
+++ api.py
@@ -7,7 +7,9 @@ def open(link):
     query = dict.keys()[1]
     pages = dict[query]["pages"]
     text = pages.keys()[0]
-    print dict[query]["pages"][text]
+    l = dict[query]["pages"][text]["revisions"][0]["*"]
+    print l
+
 
 
 open("http://en.wikipedia.org/w/api.php?action=query&prop=revisions&format=json&rvprop=content&rvlimit=1&titles=Discovered_attack")
