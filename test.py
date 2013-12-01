9d4d900c93eb12fad80aa044ec61d966275cd69b
Hanson <hanson@ubuntu.(none)>
2013-12-01 14:20:58 -0500

    testing commit

--- test.py
+++ test.py
@@ -7,4 +7,4 @@ import sys
 page = urllib2.urlopen("http://en.wikipedia.org/w/api.php?action=query&prop=revisions&format=json&rvprop=content&rvlimit=1&titles=Sicilian_Defence")
 
 test = json.load(page)
-print(test)
+print test
