9b5e34b28945443473410db103159b29d2a9e5f1
Hanson <hanson@ubuntu.(none)>
2013-12-01 13:40:01 -0500

    Figured out how to find chess notation sorta

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 import json
 import urllib2
+import unicodedata
 
 def open(link):
     page = urllib2.urlopen(link)
@@ -7,9 +8,14 @@ def open(link):
     query = dict.keys()[1]
     pages = dict[query]["pages"]
     text = pages.keys()[0]
-    l = dict[query]["pages"][text]["revisions"][0]["*"]
-    print l
-
+    l = pages[text]["revisions"][0]["*"]
+    a = l.find("1.")
+    i = 0
+    while i < 5:
+        print l[a]
+        i = i + 1
+        a = a + 1
+    
 
 
 open("http://en.wikipedia.org/w/api.php?action=query&prop=revisions&format=json&rvprop=content&rvlimit=1&titles=Discovered_attack")
