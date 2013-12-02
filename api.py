d8ec37c96bc230e4f7b879b9e2be4918bbfd057a
Hanson <hanson.lin1@gmail.com>
2013-12-01 19:30:28 -0500

    condensed slightly

--- api.py
+++ api.py
@@ -30,10 +30,10 @@ def findNotation(link):
             temp = a
             a = a + 1
             while b != 0:
-                if (l[a] > '9' and l[a] < 'A') or (l[a] > 'A' and l[a] < 'Z') or (l[a] > 'Z' and l[a] < 'a') or (l[a] > 'a' and l[a] < 'x') or l[a] > 'x' or (l[a] < '0' and l[a] > '.') or (l[a] < '.' and l[a] > ' ') or l[a] < ' ' or l[a] == '}':
-                    if (l[a+1] > '9' and l[a+1] < 'A') or (l[a+1] > 'A' and l[a+1] < 'Z') or (l[a+1] > 'Z' and l[a+1] < 'a') or (l[a+1] > 'a' and l[a+1] < 'x') or l[a+1] > 'x' or (l[a+1] < '0' and l[a+1] > '.') or (l[a+1] < '.' and l[a+1] > ' ') or l[a+1] < ' ' or l[a+1] == '}':
-                        if (l[a+2] > '9' and l[a+2] < 'A') or (l[a+2] > 'A' and l[a+2] < 'Z') or (l[a+2] > 'Z' and l[a+2] < 'a') or (l[a+2] > 'a' and l[a+2] < 'x') or l[a+2] > 'x' or (l[a+2] < '0' and l[a+2] > '.') or (l[a+2] < '.' and l[a+2] > ' ') or l[a+2] < ' ' or l[a+2] == '}':
-                            if (l[a+3] > '9' and l[a+3] < 'A') or (l[a+3] > 'A' and l[a+3] < 'Z') or (l[a+3] > 'Z' and l[a+3] < 'a') or (l[a+3] > 'a' and l[a+3] < 'x') or l[a+3] > 'x' or (l[a+3] < '0' and l[a+3] > '.') or (l[a+3] < '.' and l[a+3] > ' ') or l[a+3] < ' ' or l[a+3] == '}':
+                if (l[a] > '9' and  l[a] < 'x') or l[a] > 'x' or (l[a] < '0' and l[a] > '.') or (l[a] < '.' and l[a] > ' ') or l[a] < ' ':
+                    if (l[a+1] > '9' and l[a+1] < 'x') or l[a+1] > 'x' or (l[a+1] < '0' and l[a+1] > '.') or (l[a+1] < '.' and l[a+1] > ' ') or l[a+1] < ' ':
+                        if (l[a+2] > '9' and l[a+2] < 'x') or l[a+2] > 'x' or (l[a+2] < '0' and l[a+2] > '.') or (l[a+2] < '.' and l[a+2] > ' ') or l[a+2] < ' ':
+                            if (l[a+3] > '9' and l[a+3] < 'x') or l[a+3] > 'x' or (l[a+3] < '0' and l[a+3] > '.') or (l[a+3] < '.' and l[a+3] > ' ') or l[a+3] < ' ':
                                 b = 0
                 if l[a] == ',' or l[a] == '(':
                     b = 0
