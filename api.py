6e1803dceaade01f273c5d7ac532cf58722c3e49
Hanson Lin <hanson.lin1@gmail.com>
2013-12-05 11:39:12 -0500

    Update api.py
    Seemed to have fixed the infinite while loop

--- api.py
+++ api.py
@@ -33,11 +33,11 @@ def findNotation(link):
             a = a + 1
             #checks to see if the characters in question are actually 
             #chess notation or if they are some other sort of list or whatnot
-             while b != 0:
-                if (l[a] >= '9' and  l[a] <= 'x') or l[a] >= 'x' or (l[a] <= '0' and l[a] >= '.') or (l[a] <= '.' and l[a] >= ' ') or l[a] <= ' ':
-                    if (l[a+1] >= '9' and l[a+1] <= 'x') or l[a+1] >= 'x' or (l[a+1] <= '0' and l[a+1] >= '.') or (l[a+1] <= '.' and l[a+1] >= ' ') or l[a+1] <= ' ':
-                        if (l[a+2] >= '9' and l[a+2] <= 'x') or l[a+2] >= 'x' or (l[a+2] <= '0' and l[a+2] >= '.') or (l[a+2] <= '.' and l[a+2] >=' ') or l[a+2] <= ' ':
-                            if (l[a+3] >= '9' and l[a+3] <= 'x') or l[a+3] >= 'x' or (l[a+3] <= '0' and l[a+3] >= '.') or (l[a+3] <= '.' and l[a+3] >= ' ') or l[a+3] <= ' ':
+            while b != 0:
+                if (l[a] > '9' and  l[a] < 'x') or l[a] > 'x' or (l[a] < '0' and l[a] > '.') or (l[a] < '.' and l[a] > ' ') or l[a] < ' ':
+                    if (l[a+1] > '9' and l[a+1] < 'x') or l[a+1] > 'x' or (l[a+1] < '0' and l[a+1] > '.') or (l[a+1] < '.' and l[a+1] > ' ') or l[a+1] < ' ':
+                        if (l[a+2] > '9' and l[a+2] < 'x') or l[a+2] > 'x' or (l[a+2] < '0' and l[a+2] > '.') or (l[a+2] < '.' and l[a+2] > ' ') or l[a+2] < ' ':
+                            if (l[a+3] > '9' and l[a+3] < 'x') or l[a+3] > 'x' or (l[a+3] < '0' and l[a+3] > '.') or (l[a+3] < '.' and l[a+3] > ' ') or l[a+3] <= ' ':
                                 b = 0
                 if l[a] == ',' or l[a] == '(':
                     b = 0
@@ -45,7 +45,7 @@ def findNotation(link):
                     break
                 #This breaks the function once you hit the next move
                 if l[a] >= '0' and l[a] <= '9':
-                    if l[a+1] == '.' and (l[a+2] >= 'a' and l[a+2] <= 'z' or l[a+2] >= 'A' and l[a+2] <= 'Z' or l[a+2] == ' ' ):
+                    if l[a+1] == '.' and (l[a+2] >= 'a' and l[a+2] <= 'z' or l[a+2] >= 'A' and l[a+2] <= 'Z' or l[a+2] == ' '):
                         break
                 #This breaks if there extra "."s in the notation but also allows
                 #for "..."s in the chess notation
