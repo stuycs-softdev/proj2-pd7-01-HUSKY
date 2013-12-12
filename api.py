5da6e8ae0978efdcf7c2127941463e5c06975ab7
Nancy Dai <babycarr6193@gmail.com>
2013-12-12 12:49:26 -0500

    changed to clean up scraper

--- api.py
+++ api.py
@@ -2,12 +2,42 @@ import json
 import urllib2
 import unicodedata
 
-#finds all the algebraic notation in the article, returns a list
+def isMove(string):
+    num = False
+    alpha = False
+    other = False
+    for x in string:
+        if x.isdigit():
+            num = True
+        if x.isalpha():
+            alpha = True
+        if (x >= '!' and x <= '*') or (x > '9' and x < '?') or (x > 'Z' and x < 'a') or (x > 'z' and x <= '~') or x == '@' or x == '-' or x == '/':
+            other = True
+    return num and alpha and not other
+
+def cleanMoves(string):
+    if string[1:2] == '.':
+        string = string[2:]
+    i = len(string) - 1
+    while (1):
+        s = string[i:i+1]
+        if (s > ' ' and s < '0') or (s > '9' and s < 'A'):
+            string = string[:i]
+            i = i - 1
+        else:
+            break
+    i = 0
+    while (1):
+        s = string[i:i+1]
+        if (s > ' ' and s < '0') or (s > '9' and s < 'A'):
+            string = string[i+1:]
+        else:
+            break
+    return string
+
 def findNotation(link):
-    #makes the link compatible with mediawiki api
     path = link[link.find("/wiki/")+6:]
     page = urllib2.urlopen("http://en.wikipedia.org/w/api.php?action=query&prop=revisions&format=json&rvprop=content&rvlimit=1&titles=" + path)
-    #gets the article text out of the page returned by mediawiki
     dict = json.load(page)
     query = dict.keys()[1]
     pages = dict[query]["pages"]
@@ -17,49 +47,28 @@ def findNotation(link):
     d = f.__len__()
     e = f.find('1.')   
     l = f[e:]
-    #finds the algebraic notation, adds it to string ret
-    ret = ""
-    x = 1
-    while (x < 100):
-        #looks for the number of the turn (written as '1.', '2.', etc.)
-        a = l.find(str(x)+'.')
-        #b's only purpose is to break the function via while loops
-        b = 1
-        if a == -1 or l[a-1] >= 'a' and l[a-1] <= 'z':
-            x = x + 1
-        else:
-            ret = ret + ' ' + l[a]
-            temp = a
-            a = a + 1
-            #checks to see if the characters in question are actually 
-            #chess notation or if they are some other sort of list or whatnot
-            while b != 0:
-                if (l[a] > '9' and  l[a] < 'x') or l[a] > 'x' or (l[a] < '0' and l[a] > '.') or (l[a] < '.' and l[a] > ' ') or l[a] < ' ':
-                    if (l[a+1] > '9' and l[a+1] < 'x') or l[a+1] > 'x' or (l[a+1] < '0' and l[a+1] > '.') or (l[a+1] < '.' and l[a+1] > ' ') or l[a+1] < ' ':
-                        if (l[a+2] > '9' and l[a+2] < 'x') or l[a+2] > 'x' or (l[a+2] < '0' and l[a+2] > '.') or (l[a+2] < '.' and l[a+2] > ' ') or l[a+2] < ' ':
-                            if (l[a+3] > '9' and l[a+3] < 'x') or l[a+3] > 'x' or (l[a+3] < '0' and l[a+3] > '.') or (l[a+3] < '.' and l[a+3] > ' ') or l[a+3] <= ' ':
-                                b = 0
-                if l[a] == ',' or l[a] == '(':
-                    b = 0
-                if b == 0:
-                    break
-                #This breaks the function once you hit the next move
-                if l[a] >= '0' and l[a] <= '9':
-                    if l[a+1] == '.' and (l[a+2] >= 'a' and l[a+2] <= 'z' or l[a+2] >= 'A' and l[a+2] <= 'Z' or l[a+2] == ' '):
-                        break
-                #This breaks if there extra "."s in the notation but also allows
-                #for "..."s in the chess notation
-                if l[a] == '.':
-                    if l[a-1] != str(x) and l[a-1] != '.' and l[a+1] != '.':
-                        break
-                ret = ret + l[a]
-                a = a + 1
-                #substrings l so that you will not read the same move over again
-                l = l[:temp+1] + ' ' + l[temp+2:]
-    #breaks up the string of notation into a list, returns it
-    ret = ret.split()
-    print ret
+    l = l.split()
+    moves = []    
+    for x in l:
+        if isMove(x):
+            moves.append(x)
+    x = 0
+    final = []
+    m1 = ''
+    m2 = ''
+    while (x < len(moves) - 1):
+        m1 = moves[x][1:2]
+        m2 = moves[x+1][1:2]
+        if m1 == '.':
+            if m2 != '.':
+                chess = moves[x] + ' ' + moves[x+1]
+                final.append(cleanMoves(chess))
+                x = x + 2
+            elif m2 == '.':
+                final.append(cleanMoves(moves[x]))
+                final.append(cleanMoves(moves[x+1]))
+                x = x + 2
+            else:
+                x = x + 1
+    return final
 
-
-
-findNotation("http://en.wikipedia.org/wiki/Discovered_attack")
